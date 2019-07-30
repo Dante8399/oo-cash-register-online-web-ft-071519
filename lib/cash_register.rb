67384962512f2193ad6ba3bf09c0c79567163adb
Dante Spencer <spencerdantel@gmail.com>
2019-07-30 13:31:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
+class CashRegister
+  attr_accessor :cahs_register
+  attr_accessor :cash_register_with_discount
+  
+  def initialize
+    @total = 0
+  end
+  
+  def discount 
+    discount = self.cash_register_with_discount *
+  end
 
+    
+    
+  
+  
